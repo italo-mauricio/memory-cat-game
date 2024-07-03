b7df1de9cfe3dd6987ce7cf60119b10edacc7060
italo <italomauricio98@gmail.com>
2024-07-02 22:43:02 -0300

    initial commit: Created the base flutter for memory cat game#1

---
+++ memorygame/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web and Mobile App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Web and Mobile App'),
+      ),
+      body: Center(
+        child: Text('Hello, Flutter!'),
+      ),
+    );
+  }
+}
