8687bc69325b7d66ef3455b13585726bdc659734
DayXL <day.luc.xav0606@gmail.com>
2024-07-20 18:37:52 -0300

    adicionado botões para ver ranking

--- memorygame/lib/screens/initial_screen.dart
+++ memorygame/lib/screens/initial_screen.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:memorygame/screens/ranking.dart';
 
 class InitialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromARGB(255, 252, 252, 252), // Cor de fundo personalizada para InitialScreen
-      child: Center(
-        child: Text(
-          'Initial Screen',
-          style: TextStyle(fontSize: 24, color: Colors.red),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 252, 252, 252), // Cor de fundo personalizada para InitialScreen
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RankingScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 60), 
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15), 
+                textStyle: TextStyle(fontSize: 18), 
+              ),
+              child: const Text('Pontuações'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RankingScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 60), 
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                textStyle: TextStyle(fontSize: 18),
+              ),
+              child: const Text('Jogar'),
+            ),
+          ],
         ),
       ),
     );
