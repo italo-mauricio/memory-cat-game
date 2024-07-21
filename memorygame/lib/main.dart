077cd8dde59c269a4d682b20b71b81121079b676
DayXL <day.luc.xav0606@gmail.com>
2024-07-21 11:57:22 -0300

    movido conexão com o bd

--- memorygame/lib/main.dart
+++ memorygame/lib/main.dart
@@ -5,7 +5,6 @@ import 'firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import './services/firebase.dart';
 
-ConnetionFirebase connection = ConnetionFirebase();
 
 Future<void> main() async {
   
