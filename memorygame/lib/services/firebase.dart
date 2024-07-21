ec1156cae48c5167f0ac05595585361aed901704
DayXL <day.luc.xav0606@gmail.com>
2024-07-21 11:56:37 -0300

    adicionado consulta ao bd

--- memorygame/lib/services/firebase.dart
+++ memorygame/lib/services/firebase.dart
@@ -1,14 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 FirebaseFirestore db = FirebaseFirestore.instance;
-
 class ConnetionFirebase {
 
   Future<void> getModalidade() async {
     try {
       QuerySnapshot querySnapshot = await db.collection('modalidade').get();
 
-      // Itera pelos documentos
       for (var doc in querySnapshot.docs) {
         print('ID: ${doc.id}');
         print('Nome: ${doc['nome']}');
@@ -19,4 +17,38 @@ class ConnetionFirebase {
     }
   }
 
+  Future<List<Map<String, dynamic>>> getPontuacao() async {
+    List<Map<String, dynamic>> results = [];
+
+    try {
+      QuerySnapshot querySnapshot = await db.collection('jogador_partida').get();
+
+      for (var pontuacao in querySnapshot.docs) {
+        String idJogador = pontuacao['id_jogador'];
+
+        DocumentSnapshot jogador = await db.collection('jogador').doc(idJogador).get();
+
+        if (jogador.exists) {
+          DocumentSnapshot jogadorDoc = await db.collection('jogador').doc(idJogador).get();
+            results.add({
+              'pontuacao': pontuacao['pontuacao'],
+              'nome_jogador': jogadorDoc['nick'],
+            });
+
+        } else {
+          print('Jogador não encontrado.');
+        }
+      }
+
+      results.sort((a, b) => b['pontuacao'].compareTo(a['pontuacao']));
+      return results;
+
+    } catch (e) {
+      print('Erro ao consultar pontuações e jogadores: $e');
+      return [];
+    }
+  }
+
 }
+
+ConnetionFirebase connection = ConnetionFirebase();
