fdb2ffd694a936663dbcf16fa8b2c60f02cedbb5
DayXL <day.luc.xav0606@gmail.com>
2024-08-10 10:21:35 -0300

    colocado a requisição da pontuação na tela dos rankings

--- memorygame/lib/screens/ranking.dart
+++ memorygame/lib/screens/ranking.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:memorygame/components/myAppBar.dart';
-class RankingScreen extends StatelessWidget {
-  final Color backgroundColor;
-  final List<Map<String, dynamic>> rankings;
+import 'package:memorygame/services/firebase.dart';
 
-  RankingScreen({
-    this.backgroundColor = const Color(0xFFF2D680),
-    required this.rankings,
-  });
+class RankingScreen extends StatelessWidget {
+  final Color backgroundColor = const Color(0xFFF2D680);
+
+  Future<List<Map<String, dynamic>>> fetchRankings() async {
+    return await connection.getPontuacao();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,33 +16,68 @@ class RankingScreen extends StatelessWidget {
         title: 'Ranking',
       ),
       backgroundColor: backgroundColor,
-      body: ListView.builder(
-        itemCount: rankings.length,
-        itemBuilder: (context, index) {
-          final item = rankings[index];
-          return ListTile(
-            leading: CircleAvatar(
-              child: Text((index + 1).toString(), 
-              style: const TextStyle(
-                  fontFamily: 'Cattie',
-                  fontSize: 30,
-                  color: Color.fromARGB(153, 2, 2, 2),
-                ),),
-            ),
-            title: Text(item['nome_jogador'], 
-            style: const TextStyle(
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: fetchRankings(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(
+                child: Text(
+              'Erro ao carregar pontuação',
+              style: TextStyle(
                 fontFamily: 'Cattie',
-                fontSize: 30,
-                color: Color.fromARGB(153, 2, 2, 2),
-              ),),
-            trailing: Text(item['pontuacao'].toString(),
-              style: const TextStyle(
-                  fontFamily: 'Cattie',
-                  fontSize: 30,
-                  color: Color.fromARGB(153, 2, 2, 2),
-                ),
+                fontSize: 18,
+                color: Colors.black87,
               ),
-          );
+            ));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(
+              child: Text(
+              'Sem pontuações para exibir',
+              style: TextStyle(
+                fontFamily: 'Cattie',
+                fontSize: 18,
+                color: Colors.black87,
+              ),
+            ));
+          } else {
+            final rankings = snapshot.data!;
+            return ListView.builder(
+              itemCount: rankings.length,
+              itemBuilder: (context, index) {
+                final item = rankings[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Text(
+                      (index + 1).toString(),
+                      style: const TextStyle(
+                        fontFamily: 'Cattie',
+                        fontSize: 30,
+                        color: Color.fromARGB(153, 2, 2, 2),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    item['nome_jogador'],
+                    style: const TextStyle(
+                      fontFamily: 'Cattie',
+                      fontSize: 30,
+                      color: Color.fromARGB(153, 2, 2, 2),
+                    ),
+                  ),
+                  trailing: Text(
+                    item['pontuacao'].toString(),
+                    style: const TextStyle(
+                      fontFamily: 'Cattie',
+                      fontSize: 30,
+                      color: Color.fromARGB(153, 2, 2, 2),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
         },
       ),
     );
