b1e4087372b2f54942bd0284f658ef2428007bc1
DayXL <day.luc.xav0606@gmail.com>
2024-08-17 09:18:18 -0300

    resolvido cartas sempre mudarem quando viravam

--- memorygame/lib/components/cardList.dart
+++ memorygame/lib/components/cardList.dart
@@ -1,10 +1,9 @@
 import 'catcard.dart';
 import 'package:flutter/material.dart';
 import 'package:memorygame/models/memorygame.dart';
-class CardList extends StatelessWidget {
-  final int numCards;
-  final MemoryGameManager _gameManager = MemoryGameManager();
 
+class CardList extends StatefulWidget {
+  final int numCards;
   CardList({required this.numCards}) {
     if (numCards % 2 != 0) {
       throw Exception('Number of cards must be even.');
@@ -12,8 +11,22 @@ class CardList extends StatelessWidget {
   }
 
   @override
+  State<CardList> createState() => _CardListState();
+}
+
+class _CardListState extends State<CardList> {
+  final MemoryGameManager _gameManager = MemoryGameManager();
+
+  late List<int> _images;
+
+  @override
+  void initState() {
+    super.initState();
+    _images = _gameManager.generateImageList(widget.numCards); // Gera as imagens uma vez
+  }
+
+  @override
   Widget build(BuildContext context) {
-    List<int> images = _gameManager.generateImageList(numCards);
 
     return GridView.builder(
       gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
@@ -22,11 +35,10 @@ class CardList extends StatelessWidget {
         crossAxisSpacing: 4.0,
         childAspectRatio: 1.0, // Para manter os cards quadrados
       ),
-      itemCount: numCards,
+      itemCount: widget.numCards,
       itemBuilder: (context, index) {
         return CatCard(
-          statusCode: images[index],
-          index: index,
+          statusCode: _images[index],
         );
       },
     );
