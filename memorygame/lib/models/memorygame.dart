c41ca417ff0885fc679c8cff8cb025dd5919104b
DayXL <day.luc.xav0606@gmail.com>
2024-08-17 21:48:05 -0300

    adicionado mais imagens, agora pega embaralhado

--- memorygame/lib/models/memorygame.dart
+++ memorygame/lib/models/memorygame.dart
@@ -1,9 +1,15 @@
 import 'dart:math';
 
 class MemoryGameManager {
-  final List<int> imagePaths = [100, 102, 200, 201, 204, 214, 400, 404];
+  final List<int> imagePaths = [100, 102, 200, 201, 202, 204, 205, 206, 207,300,
+    301,302,303,304,305,307,308,400,401,402,403,404,405,406,407,408,409,410,411,
+    412,413,414,415,416,417,418,421,422,423,424,426,428,429,431,451,500,501,502,
+    503,504,507,508,510
+  ];
+  
+  List<Map<String, dynamic>> generateImageList(int numCards) {
+    imagePaths.shuffle();
 
-   List<Map<String, dynamic>>  generateImageList(int numCards) {
     if (numCards % 2 != 0) {
       throw Exception('Number of cards must be even.');
     }
