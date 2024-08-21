aa58757341d1602bf5999b513248b3fc3093318d
DayXL <day.luc.xav0606@gmail.com>
2024-08-21 09:44:53 -0300

    tirado linhas desnecessaria

--- memorygame/lib/main.dart
+++ memorygame/lib/main.dart
@@ -3,8 +3,6 @@ import 'routes/initial-app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import './services/firebase.dart';
-
 
 Future<void> main() async {
   
